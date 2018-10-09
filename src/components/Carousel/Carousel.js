c55a725eddc395563bd6d6ab8b259dc5edd94720
John Tran <johntt@uci.edu>
2018-10-08 20:37:55 -0700

    updated carousel options

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,29 +1,30 @@
 import React from "react";
 import "./Carousel.css";
 import { Carousel } from "react-responsive-carousel";
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class Photosel extends React.Component{
     render() {
         return(
-            <Carousel showArrows={true} showIndicators={true}>
+            <Carousel showArrows={true} showIndicators={true} showThumbs={false} infiniteLoop={true} autoPlay={true} showStatus={false}>
                 <div>
-                    <img src="/images/slide1.png" />
+                    <img className="slides" src="/images/slide1.png" />
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                    <img src="/images/slide2.png" />
+                    <img className="slides" src="/images/slide2.png" />
                     <p className="legend">Legend 2</p>
                 </div>
                 <div>
-                    <img src="/images/slide3.jpg" />
+                    <img className="slides" src="/images/slide3.jpg" />
                     <p className="legend">Legend 3</p>
                 </div>
                 <div>
-                    <img src="/images/slides4.jpg" />
+                    <img className="slides" src="/images/slide4.jpg" />
                     <p className="legend">Legend 4</p>
                 </div>
                 <div>
-                    <img src="/images/slide5.jpg" />
+                    <img className="slides" src="/images/slide5.jpg" />
                     <p className="legend">Legend 5</p>
                 </div>
             </Carousel>
